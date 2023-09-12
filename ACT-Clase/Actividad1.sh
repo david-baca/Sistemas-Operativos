b4386e4174797fcac7d61c886877fe1e8155f3fb
david-baca <125657162+david-baca@users.noreply.github.com>
2023-09-12 23:23:04 +0000

    Add files via upload

---
+++ ACT-Clase/Actividad1.sh
@@ -0,0 +1,12 @@
+#1/bin/bash
+mkdir uno
+mkdir dos
+mkdir tres
+mkdir cuatro
+mkdir cinco
+mkdir seis
+touch ./uno/text.txt
+touch ./tres/file3.txt
+#touch ./seis/holamundo.sh
+bash ./seis/holamundo.sh
+bash ./seis/Presentacion.sh
